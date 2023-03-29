fe7c593aedf91863d9fdba03fc1e29599c79b107
chrmanohar <85059078+chrmanohar@users.noreply.github.com>
2023-03-29 11:21:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! pollSCM1");
         System.out.println("Hello, World! pollSCM2"); 
-        System.out.println("Hello, World! pollSCM"); 
+        System.out.println("Hello, World! pollSCM3"); 
         System.out.println("Hello, World! pollSCM"); 
         System.out.println("Hello, World! webhook1"); 
     }
