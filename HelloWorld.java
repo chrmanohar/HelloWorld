5cc8ed15a45016c4e62894d4635acc7fa8fbb425
chrmanohar <85059078+chrmanohar@users.noreply.github.com>
2023-03-29 11:20:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! pollSCM");
+        System.out.println("Hello, World! pollSCM1");
         System.out.println("Hello, World! pollSCM"); 
         System.out.println("Hello, World! pollSCM"); 
         System.out.println("Hello, World! pollSCM"); 
